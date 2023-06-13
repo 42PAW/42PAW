d1a5f6b47d21146d6cffc7a1910b3c0c5efea7c5
Ssuamje <side2850@naver.com>
2023-06-13 16:27:52 +0900

    [BE] 프로젝트 생성

---
+++ backend/src/main/java/proj/pet/PetsApplication.java
@@ -0,0 +1,13 @@
+package proj.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsApplication.class, args);
+	}
+
+}

fd7273e5c386d006d7a6df466532b710a88faf26
hyungseok <eric.nohhyungseok@gmail.com>
2023-08-01 16:01:02 +0900

    [FE]: FEAT: Toast BanModal 닫힐 시 적용 테스트

--- frontend/src/components/modals/BanModal/BanModal.tsx
+++ frontend/src/components/modals/BanModal/BanModal.tsx
@@ -6,12 +6,22 @@ import { useRecoilState } from "recoil";
 import { ICurrentModalStateInfo } from "../../../types/interface/modal.interface";
 import { banUserInfoState } from "../../../recoil/atom";
 import { IBanUserInfo } from "../../../types/interface/user.interface";
+import useModal from "../../../hooks/useModal";
+import useToaster from "../../../hooks/useToaster";
 
 const BanModal: React.FC = () => {
   const [currentOpenModal] = useRecoilState<ICurrentModalStateInfo>(
     currentOpenModalState
   );
   const [banUserInfo] = useRecoilState<IBanUserInfo>(banUserInfoState);
+  const { closeModal } = useModal();
+  const { popToast } = useToaster();
+
+  const handleOnClick = () => {
+    closeModal(ModalType.BAN);
+    popToast(`${banUserInfo.userName} 님이 차단됐습니다.`);
+  };
+
   return (
     <ModalLayout modalName={ModalType.BAN} isOpen={currentOpenModal.banModal}>
       <WrapperStyled>
@@ -20,7 +30,7 @@ const BanModal: React.FC = () => {
         <ContentStyled>
           {banUserInfo.userName} 님을 차단하시겠습니까?
         </ContentStyled>
-        <button>차단</button>
+        <button onClick={handleOnClick}>차단</button>
       </WrapperStyled>
     </ModalLayout>
   );

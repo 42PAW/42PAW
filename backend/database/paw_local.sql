ea3c4db47c1e1b516e213ed52d5012947e262118
Ssuamje <side2850@naver.com>
2023-08-07 18:52:43 +0900

    [BE] FIX: DB 스키마 순서 변경

--- backend/database/paw_local.sql
+++ backend/database/paw_local.sql
@@ -1,20 +1,20 @@
 -- auto-generated definition
 create table member
 (
-    created_at          datetime(6)                                                                                                                                                                                                                                                                                                                                not null,
-    deleted_at          datetime(6)                                                                                                                                                                                                                                                                                                                                null,
     id                  bigint auto_increment
         primary key,
-    nickname_updated_at datetime(6)                                                                                                                                                                                                                                                                                                                                not null,
-    nickname            varchar(12)                                                                                                                                                                                                                                                                                                                                not null,
-    statement           varchar(30)                                                                                                                                                                                                                                                                                                                                null,
-    country             varchar(32)                                                                                                                                                                                                                                                                                                                                not null,
-    language            varchar(32)                                                                                                                                                                                                                                                           not null,
     oauth_type          varchar(32)                                                                                                                                                                                                                                                                                                               not null,
-    role                varchar(32)                                                                                                                                                                                                                                                                                                                     not null,
     oauth_id            varchar(255)                                                                                                                                                                                                                                                                                                                               not null,
     oauth_name          varchar(255)                                                                                                                                                                                                                                                                                                                               null,
+    nickname            varchar(12)                                                                                                                                                                                                                                                                                                                                not null,
+    nickname_updated_at datetime(6)                                                                                                                                                                                                                                                                                                                                not null,
+    country             varchar(32)                                                                                                                                                                                                                                                                                                                                not null,
+    language            varchar(32)                                                                                                                                                                                                                                                           not null,
+    statement           varchar(30)                                                                                                                                                                                                                                                                                                                                null,
     profile_image_url   varchar(255)                                                                                                                                                                                                                                                                                                                               null
+    role                varchar(32)                                                                                                                                                                                                                                                                                                                     not null,
+    created_at          datetime(6)                                                                                                                                                                                                                                                                                                                                not null,
+    deleted_at          datetime(6)                                                                                                                                                                                                                                                                                                                                null,
 );
 -- auto-generated definition
 create table animal_category
@@ -26,9 +26,9 @@ create table animal_category
 -- auto-generated definition
 create table block
 (
-    blocked_at       datetime(6) not null,
     member_id        bigint      not null,
     target_member_id bigint      not null,
+    blocked_at       datetime(6) not null,
     primary key (member_id, target_member_id),
     constraint FKh5v5s5r99ghk8xqk8hot1a9r
         foreign key (target_member_id) references member (id),
@@ -38,22 +38,22 @@ create table block
 -- auto-generated definition
 create table board
 (
-    created_at    datetime(6)                not null,
-    deleted_at    datetime(6)                null,
     id            bigint auto_increment
         primary key,
     member_id     bigint                     not null,
-    updated_at    datetime(6)                not null,
-    visible_scope varchar(32) not null,
     content       varchar(255)               not null,
+    visible_scope varchar(32) not null,
+    created_at    datetime(6)                not null,
+    deleted_at    datetime(6)                null,
+    updated_at    datetime(6)                not null,
     constraint FKsds8ox89wwf6aihinar49rmfy
         foreign key (member_id) references member (id)
 );
 -- auto-generated definition
 create table board_category_filter
 (
-    consumer_id bigint not null,
     provider_id bigint not null,
+    consumer_id bigint not null,
     primary key (consumer_id, provider_id),
     constraint FK1prxwta0lkm2dfs1eykf1adal
         foreign key (provider_id) references animal_category (id),
@@ -63,10 +63,10 @@ create table board_category_filter
 -- auto-generated definition
 create table board_media
 (
-    media_index int                     not null,
-    board_id    bigint                  not null,
     id          bigint auto_increment
         primary key,
+    board_id    bigint                  not null,
+    media_index int                     not null,
     media_type  varchar(32) not null,
     media_url   varchar(255)            not null,
     constraint FKf8tyql3th2klhxwlfi1nn5a3f
@@ -75,12 +75,12 @@ create table board_media
 -- auto-generated definition
 create table comment
 (
-    board_id   bigint      not null,
-    created_at datetime(6) not null,
-    deleted_at datetime(6) null,
     id         bigint auto_increment
         primary key,
+    board_id   bigint      not null,
     member_id  bigint      not null,
+    created_at datetime(6) not null,
+    deleted_at datetime(6) null,
     constraint FKlij9oor1nav89jeat35s6kbp1
         foreign key (board_id) references board (id),
     constraint FKmrrrpi513ssu63i2783jyiv9m
@@ -89,9 +89,9 @@ create table comment
 -- auto-generated definition
 create table follow
 (
-    followed_at      datetime(6) not null,
     member_id        bigint      not null,
     target_member_id bigint      not null,
+    followed_at      datetime(6) not null,
     primary key (member_id, target_member_id),
     constraint FKbv9fpsq0i3foaq6800gwday36
         foreign key (target_member_id) references member (id),
@@ -101,8 +101,8 @@ create table follow
 -- auto-generated definition
 create table member_category_filter
 (
-    consumer_id bigint not null,
     provider_id bigint not null,
+    consumer_id bigint not null,
     primary key (consumer_id, provider_id),
     constraint FKdb9dep6tmiym2lsnf06wk6ewc
         foreign key (provider_id) references animal_category (id),
@@ -112,12 +112,12 @@ create table member_category_filter
 -- auto-generated definition
 create table reaction
 (
-    board_id      bigint                not null,
-    created_at    datetime(6)           not null,
     id            bigint auto_increment
         primary key,
+    board_id      bigint                not null,
     member_id     bigint                not null,
     reaction_type varchar(32) not null,
+    created_at    datetime(6)           not null,
     constraint FK7bug6mxn1l3mq8wj67f9jxfv8
         foreign key (board_id) references board (id),
     constraint FKf0kgc48u5e6wakvieex07kk5w
@@ -126,15 +126,15 @@ create table reaction
 -- auto-generated definition
 create table report
 (
-    board_id           bigint                                                                                                                 not null,
-    comment_id         bigint                                                                                                                 not null,
-    created_at         datetime(6)                                                                                                            not null,
     id                 bigint auto_increment
         primary key,
+    board_id           bigint                                                                                                                 not null,
+    comment_id         bigint                                                                                                                 not null,
     member_id          bigint                                                                                                                 not null,
     reported_member_id bigint                                                                                                                 not null,
     reason             varchar(32) not null,
     content            varchar(255)                                                                                                           null,
+    created_at         datetime(6)                                                                                                            not null,
     constraint FK1dw2gwqqspkllnye2ylaiabqx
         foreign key (comment_id) references comment (id),
     constraint FK2mf8ehoy689ord40esx24n26e
@@ -147,11 +147,11 @@ create table report
 -- auto-generated definition
 create table scrap
 (
-    board_id   bigint      not null,
-    created_at datetime(6) not null,
     id         bigint auto_increment
         primary key,
+    board_id   bigint      not null,
     member_id  bigint      not null,
+    created_at datetime(6) not null,
     constraint FK46d2cvrgggcia4oean5lykfr
         foreign key (board_id) references board (id),
     constraint FKq0ff1jblgu8vrsh90u826qell

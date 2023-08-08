ed379511517c1ecc58b6000db4dc1064e1f1a438
Ssuamje <side2850@naver.com>
2023-08-08 18:52:43 +0900

    [COMMON] CORS 설정 config 추가

--- backend/src/main/java/proj/pet/PetsApplication.java
+++ backend/src/main/java/proj/pet/PetsApplication.java
@@ -1,9 +1,13 @@
 package proj.pet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import proj.pet.config.CorsConfig;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@Import({CorsConfig.class})
 public class PetsApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/proj/pet/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package proj.pet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("https://cabi.42seoul.io", "https://dev.cabi.42seoul.io")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+				.allowCredentials(true)
+				.maxAge(3600)
+				.allowedHeaders("*");
+	}
+}

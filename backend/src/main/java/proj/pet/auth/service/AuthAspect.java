a367cc8d0f36f1fb791255f659f1c16da413922c
Ssuamje <side2850@naver.com>
2023-08-10 16:43:53 +0900

    [BE] FEAT: NOT_REGISTERED 상태일 때 라우트 호출을 시도할 시 리디렉션 되도록 변경

--- backend/src/main/java/proj/pet/auth/service/AuthAspect.java
+++ backend/src/main/java/proj/pet/auth/service/AuthAspect.java
@@ -10,13 +10,18 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import proj.pet.auth.domain.AuthGuard;
 import proj.pet.auth.domain.CookieManager;
+import proj.pet.auth.domain.DomainProperties;
 import proj.pet.auth.domain.jwt.JwtPayload;
 import proj.pet.auth.domain.jwt.JwtProperties;
 import proj.pet.auth.domain.jwt.JwtTokenManager;
 import proj.pet.exception.ServiceException;
+import proj.pet.member.domain.MemberRole;
+
+import java.io.IOException;
 
 import static proj.pet.exception.ExceptionStatus.UNAUTHENTICATED;
 import static proj.pet.exception.ExceptionStatus.UNAUTHORIZED;
+import static proj.pet.member.domain.MemberRole.NOT_REGISTERED;
 
 @Aspect
 @Component
@@ -26,6 +31,7 @@ public class AuthAspect {
 	private final JwtTokenManager jwtTokenManager;
 	private final CookieManager cookieManager;
 	private final JwtProperties jwtProperties;
+	private final DomainProperties domainProperties;
 
 	/**
 	 * {@link AuthGuard} 어노테이션이 붙은 곳을 {@link org.aspectj.lang.annotation.Pointcut}으로 인터셉트합니다.
@@ -35,7 +41,7 @@ public class AuthAspect {
 	 * @param authGuard 인터셉트 된 해당 {@link AuthGuard} - Level을 알아낼 수 있습니다.
 	 */
 	@Before("@annotation(authGuard))")
-	public void AuthToken(AuthGuard authGuard) {
+	public void AuthToken(AuthGuard authGuard) throws IOException {
 		/**
 		 * 현재 인터셉트 된 서블릿의 {@link HttpServletRequest}를 가져옵니다.
 		 */
@@ -50,7 +56,11 @@ public class AuthAspect {
 			throw new ServiceException(UNAUTHORIZED);
 		}
 		JwtPayload jwtPayload = jwtTokenManager.createFtPayload(token);
-		if (!authGuard.level().isMatchWith(jwtPayload.getRole())) {
+		MemberRole role = jwtPayload.getRole();
+		if (role.equals(NOT_REGISTERED)) {
+			response.sendRedirect(domainProperties.getFrontendHost() + "/sign-up");
+		}
+		if (!authGuard.level().isMatchWith(role)) {
 			cookieManager.deleteCookie(response, jwtProperties.getTokenName());
 			throw new ServiceException(UNAUTHENTICATED);
 		}

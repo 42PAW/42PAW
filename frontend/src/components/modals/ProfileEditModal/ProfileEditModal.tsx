15d3fa05c77506fc1b061f9d53d37a7304f11e67
mink97 <101685319+mink97@users.noreply.github.com>
2023-08-10 11:53:19 +0900

    [FE] 프로필 뷰 모달 구현 (#32)
    * feat: [FE] FEAT: 프로필 편집 모달창 구현
    
    프로필 편집 버튼을 눌렀을 때 프로필 편집 모달이 나오는 동작 구현. 모달 내 동작은 아직 미구현...
    
    * feat: [FE] FEAT: 프로필 편집 모달창 취소 눌러야 벗어나고 그 외에는 모달 안닫도록 수정

--- frontend/src/components/modals/ProfileEditModal/ProfileEditModal.tsx
+++ frontend/src/components/modals/ProfileEditModal/ProfileEditModal.tsx
@@ -4,179 +4,133 @@ import { ModalType } from "../../../types/enum/modal.enum";
 import { currentOpenModalState } from "../../../recoil/atom";
 import { useRecoilState } from "recoil";
 import { ICurrentModalStateInfo } from "../../../types/interface/modal.interface";
+import useModal from "../../../hooks/useModal";
 import PropTypes from "prop-types";
 
-interface Props {
-  property1: "frame-16053";
-  className: any;
-  componentClassName: any;
-  hasButton: boolean;
-}
-const ProfileEditModal = ({
-  property1,
-  className,
-  componentClassName,
-  hasButton = true,
-}: Props): JSX.Element => {
-  return (
-    <WrapperStyled>
-      <div className={`property-frame-wrapper ${className}`}>
-        <div className={`component ${componentClassName}`}>
-          {hasButton && <div className="button">Button</div>}
-        </div>
-      </div>
-    </WrapperStyled>
+const profileInfo = {
+  memberName: "오덕애비",
+  nicknameUpdatedAt: "2023-01-23",
+  profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+  statement: "강아지는 항상 옳다. 오덕아 사랑해",
+  followingCount: 279,
+  followerCount: 678,
+  boardCount: 3423434347,
+};
+
+const ProfileEditModal = () => {
+  const [currentOpenModal] = useRecoilState<ICurrentModalStateInfo>(
+    currentOpenModalState
   );
-  //   const [currentOpenModal] = useRecoilState<ICurrentModalStateInfo>(
-  //     currentOpenModalState
-  //   );
+  const { closeModal } = useModal();
 
-  //   return (
-  //     <ModalLayout
-  //       modalName={ModalType.PROFILEEDIT}
-  //       isOpen={currentOpenModal.profileEditModal}
-  //     >
-  //       <WrapperStyled>
-  //         <h1>오덕애비</h1>
-  //         <ProfileImageStyled src="/src/assets/profileImage.jpg" />
-  //         <CaptionStyled>안녕하세요안녕하세요안녕하세요안녕하세요</CaptionStyled>
-  //         <ButtonContainerStyled>
-  //           <button>프로필</button>
-  //           <button>팔로우</button>
-  //         </ButtonContainerStyled>
-  //         <SubButtonContainerStyled>
-  //           <button>
-  //             <img src="/src/assets/ban.png" />
-  //             차단하기
-  //           </button>
-  //           <button>
-  //             <img src="/src/assets/report.png" />
-  //             신고하기
-  //           </button>
-  //         </SubButtonContainerStyled>
-  //       </WrapperStyled>
-  //     </ModalLayout>
-  //   );
+  return (
+    <ModalLayout
+      modalName={ModalType.PROFILEEDIT}
+      isOpen={currentOpenModal.profileEditModal}
+    >
+      <WrapperStyled>
+        <NameStyled>
+          <h1>{profileInfo.memberName}</h1>
+          <button
+            onClick={() => {
+              alert("click!");
+            }}
+          >
+            <img src="/src/assets/edit_icon.png" />
+          </button>
+        </NameStyled>
+        <ProfileImageStyled src={profileInfo.profileImage} />
+        <CaptionStyled>{profileInfo.statement}</CaptionStyled>
+        <ButtonContainerStyled>
+          <button>완료</button>
+          <button onClick={() => closeModal(ModalType.PROFILEEDIT)}>
+            취소
+          </button>
+        </ButtonContainerStyled>
+      </WrapperStyled>
+    </ModalLayout>
+  );
 };
 
-ProfileEditModal.propTypes = {
-  property1: PropTypes.oneOf(["frame-16053"]),
-  hasButton: PropTypes.bool,
-};
 const WrapperStyled = styled.div`
-  .property-frame-wrapper {
-    align-items: flex-start;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    height: 45px;
-    justify-content: center;
-    position: relative;
-    width: 89px;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 280px;
+  background-color: var(--white);
+  border-radius: 15px;
+  color: var(--grey);
+`;
 
-  .property-frame-wrapper .component {
-    align-items: flex-start;
-    background-color: #020080;
-    border-radius: 1000px;
-    display: inline-flex;
-    flex: 0 0 auto;
-    gap: 10px;
-    padding: 10px 25px;
-    position: relative;
-  }
-
-  .property-frame-wrapper .button {
-    color: #ffffff;
-    font-family: "Inter-SemiBold", Helvetica;
-    font-size: 12px;
-    font-weight: 600;
-    letter-spacing: 0;
-    line-height: normal;
-    margin-top: -1px;
-    position: relative;
+const NameStyled = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 5px;
+  h1 {
+    width: 180px;
+    font-size: 1.8rem;
+    border-bottom: 1.5px solid var(--grey);
     text-align: center;
-    width: fit-content;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    img {
+      width: 20px;
+      height: 20px;
+    }
   }
 `;
 
-// const WrapperStyled = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   width: 280px;
-//   background-color: var(--white);
-//   border-radius: 15px;
-//   color: var(--grey);
-//   h1 {
-//     font-size: 18px;
-//     margin-top: 40px;
-//     margin-bottom: 5px;
-//   }
-// `;
+const ProfileImageStyled = styled.img`
+  border-radius: 100%;
+  width: 100px;
+  margin-top: 20px;
+`;
+const CaptionStyled = styled.div`
+  display: flex; /* Use flexbox */
+  align-items: center; /* Vertically center the text */
+  justify-content: center; /* Horizontally center the text */
+  height: 50px;
+  width: 180px;
+  border: 0.5px dashed;
+  border-radius: 5px;
+  background-color: #f1f1f1;
+  margin-top: 25px;
+  color: var(--grey, #000000);
+  font-size: 1.4rem;
+  text-align: center;
+  word-break: keep-all;
+  padding-inline: 10px;
+`;
 
-// const ProfileImageStyled = styled.img`
-//   border-radius: 100%;
-//   width: 100px;
-// `;
-// const CaptionStyled = styled.div`
-//   margin-top: 25px;
-//   font-size: 13px;
-// `;
-
-// const ButtonContainerStyled = styled.div`
-//   margin-top: 30px;
-//   margin: 25px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   width: 215px;
-//   button {
-//     cursor: pointer;
-//     height: 40px;
-//     width: 100px;
-//     border-radius: 10px;
-//     border: none;
-//     &:nth-child(1) {
-//       background-color: var(--purple);
-//       color: var(--white);
-//     }
-//     &:nth-child(2) {
-//       background-color: var(--white);
-//       border: 1px solid var(--grey);
-//       color: var(--grey);
-//     }
-//     &:hover {
-//       opacity: 0.7;
-//     }
-//   }
-// `;
-
-// const SubButtonContainerStyled = styled.div`
-//   margin-top: -8px;
-//   padding-bottom: 25px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   width: 160px;
-//   button {
-//     cursor: pointer;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     border: none;
-//     background-color: var(--white);
-//     color: var(--lightgrey);
-//     font-size: 12px;
-//     width: 50%;
-//     &:hover {
-//       opacity: 0.7;
-//     }
-//   }
-//   img {
-//     width: 14px;
-//     margin-right: 5px;
-//   }
-// `;
+const ButtonContainerStyled = styled.div`
+  margin-top: 30px;
+  margin: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 215px;
+  button {
+    cursor: pointer;
+    height: 25px;
+    width: 100px;
+    border-radius: 10px;
+    border: none;
+    &:nth-child(1) {
+      background-color: var(--purple);
+      color: var(--white);
+    }
+    &:nth-child(2) {
+      background-color: var(--lightgrey);
+      //   border: 1px solid var(--lightgrey);
+      color: var(--white);
+    }
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
 
 export default ProfileEditModal;

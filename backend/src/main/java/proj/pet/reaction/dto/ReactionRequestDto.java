e7462f3d4cd5bcb4750224c5b5002eb74f79a482
Ssuamje <side2850@naver.com>
2023-08-13 21:20:07 +0900

    [BE] FEAT: 데이터 받는 구조 수정

---
+++ backend/src/main/java/proj/pet/reaction/dto/ReactionRequestDto.java
@@ -0,0 +1,10 @@
+package proj.pet.reaction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ReactionRequestDto {
+	private Long boardId;
+}
--- backend/src/main/java/proj/pet/reaction/service/ReactionFacadeServiceImpl.java
+++ backend/src/main/java/proj/pet/reaction/service/ReactionFacadeServiceImpl.java
@@ -2,6 +2,8 @@ package proj.pet.reaction.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import proj.pet.member.dto.UserSessionDto;
+import proj.pet.reaction.dto.ReactionRequestDto;
 
 @Service
 @RequiredArgsConstructor
@@ -9,11 +11,11 @@ public class ReactionFacadeServiceImpl implements ReactionFacadeService {
 
 	private final ReactionService reactionService;
 
-	@Override
-	public void createReaction(Long boardId) {
+	@Override public void createReaction(UserSessionDto userSessionDto, ReactionRequestDto reactionRequestDto) {
+
 	}
 
-	@Override
-	public void deleteReaction() {
+	@Override public void deleteReaction(UserSessionDto userSessionDto, Long boardId) {
+
 	}
 }

3b8fd5e3898ffd81bfe9e320e69baf61e647c575
Ssuamje <side2850@naver.com>
2023-08-13 21:31:49 +0900

    [BE] FEAT: 반응 생성, 삭제 API 추가

--- backend/src/main/java/proj/pet/reaction/service/ReactionFacadeServiceImpl.java
+++ backend/src/main/java/proj/pet/reaction/service/ReactionFacadeServiceImpl.java
@@ -2,6 +2,7 @@ package proj.pet.reaction.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import proj.pet.member.dto.UserSessionDto;
 import proj.pet.reaction.dto.ReactionRequestDto;
 
@@ -11,11 +12,13 @@ public class ReactionFacadeServiceImpl implements ReactionFacadeService {
 
 	private final ReactionService reactionService;
 
+	@Transactional
 	@Override public void createReaction(UserSessionDto userSessionDto, ReactionRequestDto reactionRequestDto) {
-
+		reactionService.createReaction(userSessionDto.getMemberId(), reactionRequestDto);
 	}
 
+	@Transactional
 	@Override public void deleteReaction(UserSessionDto userSessionDto, Long boardId) {
-
+		reactionService.deleteReaction(userSessionDto.getMemberId(), boardId);
 	}
 }
--- backend/src/main/java/proj/pet/reaction/service/ReactionService.java
+++ backend/src/main/java/proj/pet/reaction/service/ReactionService.java
@@ -1,5 +1,10 @@
 package proj.pet.reaction.service;
 
+import proj.pet.reaction.dto.ReactionRequestDto;
+
 public interface ReactionService {
 
+	void createReaction(Long memberId, ReactionRequestDto reactionRequestDto);
+
+	void deleteReaction(Long memberId, Long boardId);
 }

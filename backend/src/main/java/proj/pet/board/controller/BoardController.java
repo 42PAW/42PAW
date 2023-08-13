8a371f4a527c499e5f99fcc8180ce00f24eca2af
Ssuamje <side2850@naver.com>
2023-08-13 14:20:56 +0900

    [BE] FIX: 컨트롤러에 UserSessionDto 사용하도록 설정

--- backend/src/main/java/proj/pet/board/controller/BoardController.java
+++ backend/src/main/java/proj/pet/board/controller/BoardController.java
@@ -10,11 +10,8 @@ import proj.pet.board.service.BoardFacadeService;
 import proj.pet.category.domain.Species;
 import proj.pet.member.domain.UserSession;
 import proj.pet.member.dto.UserSessionDto;
-import proj.pet.member.repository.MemberRepository;
 
 import java.util.List;
-
-import static proj.pet.member.domain.MemberRole.USER;
 
 @RestController
 @RequestMapping("/v1/boards")
@@ -22,8 +19,6 @@ import static proj.pet.member.domain.MemberRole.USER;
 public class BoardController {
 
 	private final BoardFacadeService boardFacadeService;
-	// REMOVE
-	private final MemberRepository memberRepository;
 
 	@GetMapping
 	public BoardsResponseDto getMainViewBoards(
@@ -57,20 +52,17 @@ public class BoardController {
 			consumes = MediaType.MULTIPART_FORM_DATA_VALUE
 	)
 	public void createBoard(
-//			UserSession userSession,
+			@UserSession UserSessionDto userSessionDto,
 			@RequestPart(value = "mediaDataList") List<MultipartFile> mediaDataList,
 			@RequestPart(value = "categoryList") List<Species> categoryList,
 			@RequestPart(value = "content") String content) {
-		// TODO: userSession 시큐리티에서 가져오기
-		UserSessionDto userSessionDto = new UserSessionDto(1L, "hellow", USER);
 		boardFacadeService.createBoard(userSessionDto, mediaDataList, categoryList, content);
 	}
 
 	@DeleteMapping("/{boardId}")
 	public void deleteBoard(
-//			UserSession userSession,
+			@UserSession UserSessionDto userSessionDto,
 			@PathVariable("boardId") Long boardId) {
-		UserSessionDto userSession = new UserSessionDto(1L, "sanan", USER);
-		boardFacadeService.deleteBoard(userSession, boardId);
+		boardFacadeService.deleteBoard(userSessionDto, boardId);
 	}
 }

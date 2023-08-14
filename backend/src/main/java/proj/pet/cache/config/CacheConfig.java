4a46d6aad250466568549d68d8f755893271cd15
Ssuamje <side2850@naver.com>
2023-08-14 15:09:16 +0900

    [BE] FEAT: 캐시 의존성 추가

--- backend/src/main/java/proj/pet/cache/config/CacheConfig.java
+++ backend/src/main/java/proj/pet/cache/config/CacheConfig.java
@@ -1,7 +1,9 @@
 package proj.pet.cache.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ public class CacheConfig {
 
 	@Bean
 	public CacheManager cacheManager() {
+		new CaffeineCache("name", Caffeine.newBuilder().build());
 
 		SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
 		return simpleCacheManager;

387b582fb53c8593d8eef0d7465c80d32fea0206
Ssuamje <side2850@naver.com>
2023-08-16 23:52:22 +0900

    [BE] FIX: BoardDelete 테스트 간소화

--- backend/src/test/java/proj/pet/board/service/BoardServiceTest.java
+++ backend/src/test/java/proj/pet/board/service/BoardServiceTest.java
@@ -9,15 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import proj.pet.board.domain.Board;
-import proj.pet.board.domain.BoardMedia;
-import proj.pet.board.domain.BoardMediaManager;
-import proj.pet.board.domain.VisibleScope;
+import proj.pet.board.domain.*;
 import proj.pet.board.repository.BoardCategoryFilterRepository;
 import proj.pet.board.repository.BoardMediaRepository;
 import proj.pet.board.repository.BoardRepository;
 import proj.pet.category.domain.AnimalCategory;
-import proj.pet.category.domain.BoardCategoryFilter;
 import proj.pet.category.domain.Species;
 import proj.pet.category.repository.AnimalCategoryRepository;
 import proj.pet.member.domain.*;
@@ -28,8 +24,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @Transactional
@@ -114,37 +109,20 @@ class BoardServiceTest {
 		// TODO : 위의 데이터와 연동되므로, 데이터 분리 후 동일한 stubbing이 필요하다.
 		LocalDateTime now = LocalDateTime.now();
 		Member member = memberRepository.save(stubMember("sanan", MemberRole.USER, now));
-		List<AnimalCategory> categoryList = animalCategoryRepository.saveAll(List.of(
-				AnimalCategory.of(Species.CAT),
-				AnimalCategory.of(Species.DOG),
-				AnimalCategory.of(Species.ETC)));
-		MultipartFile mockImageFile = mock(MultipartFile.class);
-		MultipartFile mockVideoFile = mock(MultipartFile.class);
-		when(mockImageFile.getContentType()).thenReturn("image/png");
-		when(mockVideoFile.getContentType()).thenReturn("video/mp4");
-		List<MultipartFile> mediaDtoList = List.of(
-				mockImageFile,
-				mockImageFile,
-				mockVideoFile);
-		String content = "게시글 내용";
-		when(boardMediaManager.uploadMedia(mockImageFile)).thenReturn("imagePath");
-		when(boardMediaManager.uploadMedia(mockVideoFile)).thenReturn("videoPath");
+		Board board = boardRepository.save(Board.of(member, VisibleScope.PUBLIC, "content", now));
+		List<BoardMedia> boardMedia = boardMediaRepository.saveAll(List.of(
+				BoardMedia.of(board, "imagePath1", 0, MediaType.IMAGE),
+				BoardMedia.of(board, "imagePath2", 1, MediaType.IMAGE),
+				BoardMedia.of(board, "videoPath", 2, MediaType.VIDEO)));
+		board.addMediaList(boardMedia);
 		em.flush();
 		em.clear();
-		List<Species> speciesList = List.of(Species.CAT, Species.DOG, Species.ETC);
 
 		//when
-		Board board = boardService.createBoard(1L, speciesList, mediaDtoList, content, now);
-		em.flush();
-		em.clear();
-		List<BoardMedia> boardMediaList = boardMediaRepository.findAll();
-		assertThat(boardMediaList).hasSize(3);
-		List<BoardCategoryFilter> boardCategoryFilterList = boardCategoryFilterRepository.findAll();
-		assertThat(boardCategoryFilterList).hasSize(3);
 		boardService.deleteBoard(1L, 1L);
 
 		//then
-		then(boardMediaManager).should().deleteMediaByList(boardMediaList);
+		then(boardMediaManager).should(times(1)).deleteMediaByList(boardMedia);
 		assertThat(boardRepository.findById(1L)).isEmpty();
 		assertThat(boardMediaRepository.findAll()).isEmpty();
 		assertThat(boardCategoryFilterRepository.findAll()).isEmpty();

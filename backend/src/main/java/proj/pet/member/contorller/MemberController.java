58b6df7c70dc465be25fd3e322017301caebf71d
Ssuamje <side2850@naver.com>
2023-08-16 14:39:18 +0900

    [BE] FIX: createMember에 달린 AuthGuard제거

--- backend/src/main/java/proj/pet/member/contorller/MemberController.java
+++ backend/src/main/java/proj/pet/member/contorller/MemberController.java
@@ -1,33 +1,17 @@
 package proj.pet.member.contorller;
 
-import static proj.pet.auth.domain.AuthLevel.USER_OR_ADMIN;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import proj.pet.auth.domain.AuthGuard;
 import proj.pet.board.dto.BoardsPaginationDto;
 import proj.pet.member.domain.UserSession;
-import proj.pet.member.dto.MemberCreateRequestDto;
-import proj.pet.member.dto.MemberLanguageChangeRequestDto;
-import proj.pet.member.dto.MemberMyInfoResponseDto;
-import proj.pet.member.dto.MemberNicknameValidateResponseDto;
-import proj.pet.member.dto.MemberProfileChangeRequestDto;
-import proj.pet.member.dto.MemberProfileChangeResponseDto;
-import proj.pet.member.dto.MemberProfileResponseDto;
-import proj.pet.member.dto.MemberSearchPaginationDto;
-import proj.pet.member.dto.UserSessionDto;
+import proj.pet.member.dto.*;
 import proj.pet.member.service.MemberFacadeService;
+
+import static proj.pet.auth.domain.AuthLevel.USER_OR_ADMIN;
 
 @RestController
 @RequestMapping("/v1/members")
@@ -45,7 +29,6 @@ public class MemberController {
 	 * @param memberCreateRequestDto 회원 가입 정보
 	 */
 	@PostMapping(consumes = "multipart/form-data")
-	@AuthGuard(level = USER_OR_ADMIN)
 	public void createMember(
 			@UserSession UserSessionDto userSessionDto,
 			HttpServletRequest req, HttpServletResponse res,

476a2a4a7b71753ea2db45f6f0ff5233f1f61e45
jiwon <jwpark0119@gmail.com>
2023-08-17 13:26:37 +0900

    [BE] FIX: fix error when change language

--- backend/src/main/java/proj/pet/member/dto/MemberLanguageChangeRequestDto.java
+++ backend/src/main/java/proj/pet/member/dto/MemberLanguageChangeRequestDto.java
@@ -2,11 +2,13 @@ package proj.pet.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import proj.pet.member.domain.Language;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberLanguageChangeRequestDto {
 
-	private final Language language;
+	private Language language;
 }

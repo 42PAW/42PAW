c50f5f8138598ae166a869ca0bfddb82818fb29f
Ssuamje <side2850@naver.com>
2023-08-18 13:41:08 +0900

    FIX: CommentRequestDto noargesconstructor 추가

--- backend/src/main/java/proj/pet/comment/dto/CommentRequestDto.java
+++ backend/src/main/java/proj/pet/comment/dto/CommentRequestDto.java
@@ -2,10 +2,12 @@ package proj.pet.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentRequestDto {
-	private final Long boardId;
-	private final String content;
+	private Long boardId;
+	private String content;
 }

fd56b96ea79d9f008e1f9c17a7023bd034da3197
jiwon <jwpark0119@gmail.com>
2023-08-18 10:17:04 +0900

    [BE] FIX: fix follow error

--- backend/src/main/java/proj/pet/follow/dto/FollowRequestDto.java
+++ backend/src/main/java/proj/pet/follow/dto/FollowRequestDto.java
@@ -2,9 +2,11 @@ package proj.pet.follow.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FollowRequestDto {
 
 	private Long memberId;

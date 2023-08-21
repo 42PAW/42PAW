f49435ca4f39a31f808dd4d493426b3e1542da40
Ssuamje <side2850@naver.com>
2023-08-21 13:34:23 +0900

    [BE] TEST: BoardMedia 생성 유닛 테스트 작성

---
+++ backend/src/test/java/proj/pet/board/domain/BoardMediaTest.java
@@ -0,0 +1,38 @@
+package proj.pet.board.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import proj.pet.exception.DomainException;
+import proj.pet.testutil.UnitTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+class BoardMediaTest extends UnitTest {
+
+	@DisplayName("Board에 의존하고, mediaUrl, mediaType, index를 가진다.")
+	@Test
+	void constructor() {
+		Board board = mock(Board.class);
+
+		assertThat(BoardMedia.of(board, "mediaUrl", 1, MediaType.IMAGE)).isNotNull();
+
+		assertThatThrownBy(() ->
+				BoardMedia.of(null, "mediaUrl", 1, MediaType.IMAGE))
+				.isInstanceOf(DomainException.class);
+
+		assertThatThrownBy(() ->
+				BoardMedia.of(board, null, 1, MediaType.IMAGE))
+				.isInstanceOf(DomainException.class);
+
+		assertThatThrownBy(() ->
+				BoardMedia.of(board, "mediaUrl", 1, null))
+				.isInstanceOf(DomainException.class);
+
+		assertThatThrownBy(() ->
+				BoardMedia.of(board, "mediaUrl", -1, MediaType.IMAGE))
+				.isInstanceOf(DomainException.class);
+	}
+
+}

5616189ad7e04fa9a93425032a1b0f7c08e1c604
Ssuamje <side2850@naver.com>
2023-08-22 16:45:13 +0900

    [BE] FEAT: PersistHelper 추가

---
+++ backend/src/test/java/proj/pet/testutil/test/PersistHelper.java
@@ -0,0 +1,66 @@
+package proj.pet.testutil.test;
+
+import jakarta.persistence.EntityManager;
+import proj.pet.utils.domain.IdDomain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PersistHelper {
+	private final EntityManager em;
+
+	private PersistHelper(EntityManager em) {
+		this.em = em;
+	}
+
+	public static PersistHelper start(EntityManager em) {
+		return new PersistHelper(em);
+	}
+
+	public PersistHelper persist(IdDomain<?>... entities) {
+		for (IdDomain<?> entity : entities) {
+			this.em.persist(entity);
+		}
+		return this;
+	}
+
+	public PersistHelper and() {
+		return this;
+	}
+
+	public PersistHelper flush() {
+		this.em.flush();
+		return this;
+	}
+
+	public PersistHelper clear() {
+		this.em.clear();
+		return this;
+	}
+
+
+	public void flushAndClear() {
+		this.flush().clear();
+	}
+
+	public <E extends IdDomain<?>> List<E> persistAndReturn(List<E> entities) {
+		for (IdDomain<?> entity : entities) {
+			this.em.persist(entity);
+		}
+		return entities;
+	}
+
+	@SafeVarargs public final <E extends IdDomain<?>> List<E> persistAndReturn(E... entities) {
+		for (IdDomain<?> entity : entities) {
+			this.em.persist(entity);
+		}
+		return Arrays.stream(entities).collect(Collectors.toList());
+	}
+
+	public <E extends IdDomain<?>> E persistAndReturn(E entity) {
+		this.em.persist(entity);
+		return entity;
+	}
+}
+

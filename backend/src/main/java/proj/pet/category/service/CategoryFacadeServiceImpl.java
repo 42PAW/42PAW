1985c24daaf7226593a02a6b9f0d93f86e9a0a72
Ssuamje <side2850@naver.com>
2023-08-23 19:26:43 +0900

    [BE] FEAT: updateCategories API 추가

--- backend/src/main/java/proj/pet/category/service/CategoryFacadeServiceImpl.java
+++ backend/src/main/java/proj/pet/category/service/CategoryFacadeServiceImpl.java
@@ -3,15 +3,26 @@ package proj.pet.category.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import proj.pet.category.dto.CategoryResponseDto;
+import proj.pet.category.dto.CategoryUpdateRequestDto;
+import proj.pet.member.dto.UserSessionDto;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryFacadeServiceImpl implements CategoryFacadeService {
 
 	private final CategoryQueryService categoryQueryService;
+	private final CategoryService categoryService;
 
 	@Override
 	public CategoryResponseDto getCategories() {
 		return categoryQueryService.getAllCategories();
+	}
+
+	@Override public void updateMyCategories(UserSessionDto userSessionDto, CategoryUpdateRequestDto categoryUpdateRequestDto) {
+		categoryService.updateMemberCategories(userSessionDto.getMemberId(), categoryUpdateRequestDto.getCategories());
+	}
+
+	@Override public void updateBoardCategories(UserSessionDto userSessionDto, Long boardId, CategoryUpdateRequestDto categoryUpdateRequestDto) {
+		categoryService.updateBoardCategories(userSessionDto.getMemberId(), boardId, categoryUpdateRequestDto.getCategories());
 	}
 }
--- backend/src/main/java/proj/pet/category/service/CategoryService.java
+++ backend/src/main/java/proj/pet/category/service/CategoryService.java
@@ -1,4 +1,11 @@
 package proj.pet.category.service;
 
+import proj.pet.category.domain.Species;
+
+import java.util.List;
+
 public interface CategoryService {
+	void updateMemberCategories(Long memberId, List<Species> speciesList);
+
+	void updateBoardCategories(Long memberId, Long boardId, List<Species> speciesList);
 }

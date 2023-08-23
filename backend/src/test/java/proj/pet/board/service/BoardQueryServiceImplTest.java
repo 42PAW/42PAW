0aa8e4ce339771d84b2e0bbc2f5ac38495e17c00
Ssuamje <side2850@naver.com>
2023-08-24 08:52:26 +0900

    [BE] FIX: BoardQueryServiceImplTest 유닛 테스트 재작성

--- backend/src/test/java/proj/pet/board/service/BoardQueryServiceImplTest.java
+++ backend/src/test/java/proj/pet/board/service/BoardQueryServiceImplTest.java
@@ -1,148 +1,148 @@
 package proj.pet.board.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.data.domain.PageRequest;
 import proj.pet.board.domain.Board;
-import proj.pet.board.dto.BoardsPaginationDto;
 import proj.pet.board.repository.BoardRepository;
-import proj.pet.category.domain.Species;
 import proj.pet.mapper.BoardMapper;
 import proj.pet.member.domain.Member;
 import proj.pet.testutil.test.UnitTest;
+import proj.pet.testutil.testdouble.board.TestBoard;
+import proj.pet.testutil.testdouble.member.TestMember;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
 
-class BoardQueryServiceImplTest extends UnitTest {
-
+public class BoardQueryServiceImplTest extends UnitTest {
+	private static final Long IGNORE_ID = null;
+	@Mock
 	private final BoardMapper boardMapper = BoardMapper.INSTANCE;
+	@InjectMocks
+	private BoardQueryServiceImpl boardQueryService;
 	@Mock
 	private BoardRepository boardRepository;
 
-	private BoardQueryServiceImpl boardQueryService;
-
-	private static Board getMockBoardOf(Member author) {
-		Board board = mock(Board.class);
-		given(board.findBoardMediaUrls()).willReturn(List.of("url1", "url2"));
-		given(board.getCategoriesAsSpecies()).willReturn(List.of(Species.CAT, Species.DOG));
-		given(board.getMember()).willReturn(author);
-		return board;
-	}
-
-	@BeforeEach
-	void setUp() {
-		boardQueryService = new BoardQueryServiceImpl(boardRepository, boardMapper);
-	}
-
+	@DisplayName("일반 게시글을 매핑해서 컨트롤러에게 전달한다.")
 	@Test
-	@DisplayName("사용자의 ID 기준 반응, 북마크 정보를 포함한 게시글들을 가져온다.")
 	void getMainViewBoards() {
-		// given
-		Long loginUserId = 1L;
-		Member author = mock(Member.class);
-		Board board = getMockBoardOf(author);
-		given(author.getId()).willReturn(2L);
+		//given
+		Member author = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		Member loginUser = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		List<Board> boards = List.of(
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID),
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID));
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		given(boardRepository.getMainViewBoards(pageRequest)).willReturn(boards);
 
-		given(boardRepository.getMainViewBoards(PageRequest.of(0, 10)))
-				.willReturn(List.of(board, board));
-
-		// when
-		BoardsPaginationDto mainViewBoards = boardQueryService.getMainViewBoards(loginUserId, PageRequest.of(0, 10));
+		//when
+		boardQueryService.getMainViewBoards(loginUser.getId(), pageRequest);
 
 		//then
-		then(boardRepository).should().getMainViewBoards(PageRequest.of(0, 10));
-		assertThat(mainViewBoards.getResult().size()).isEqualTo(2);
-		assertThat(mainViewBoards.getResult().get(0).getCategories()).isEqualTo(List.of(Species.CAT, Species.DOG));
+		then(boardRepository).should().getMainViewBoards(pageRequest);
+		then(boardMapper).should().toBoardsResponseDto(anyList(), eq(boards.size()));
 	}
 
+	@DisplayName("핫 게시물을 매핑해서 컨트롤러에게 전달한다.")
 	@Test
-	@DisplayName("사용자의 ID 기준 반응, 북마크 정보를 포함한 HOT 게시글들을 가져온다.")
 	void getHotBoards() {
-		// given
-		Long loginUserId = 1L;
-		Member author = mock(Member.class);
-		Board board = getMockBoardOf(author);
-		given(author.getId()).willReturn(2L);
+		Member author = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		Member loginUser = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		List<Board> boards = List.of(
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID),
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID));
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		given(boardRepository.getHotBoards(pageRequest)).willReturn(boards);
 
-		given(boardRepository.getHotBoards(PageRequest.of(0, 10)))
-				.willReturn(List.of(board, board));
-
-		// when
-		BoardsPaginationDto hotViewBoards = boardQueryService.getHotBoards(loginUserId, PageRequest.of(0, 10));
+		//when
+		boardQueryService.getHotBoards(loginUser.getId(), pageRequest);
 
 		//then
-		then(boardRepository).should().getHotBoards(PageRequest.of(0, 10));
-		assertThat(hotViewBoards.getResult().size()).isEqualTo(2);
-		assertThat(hotViewBoards.getResult().get(0).getCategories()).isEqualTo(List.of(Species.CAT, Species.DOG));
+		then(boardRepository).should().getHotBoards(pageRequest);
+		then(boardMapper).should().toBoardsResponseDto(anyList(), eq(boards.size()));
 	}
 
+	@DisplayName("특정 멤버의 게시물을 매핑해서 컨트롤러에게 전달한다.")
 	@Test
-	@DisplayName("사용자의 ID 기준 반응, 북마크 정보를 포함한 특정 멤버의 게시글들을 가져온다.")
 	void getMemberBoards() {
-		// given
-		Long loginUserId = 1L;
-		Member author = mock(Member.class);
-		Board board = getMockBoardOf(author);
-		given(author.getId()).willReturn(2L);
+		Member author = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		Member loginUser = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		List<Board> boards = List.of(
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID),
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID));
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		given(boardRepository.getMemberBoards(author.getId(), pageRequest)).willReturn(boards);
 
-		given(boardRepository.getMemberBoards(author.getId(), PageRequest.of(0, 10)))
-				.willReturn(List.of(board, board));
-
-		// when
-		BoardsPaginationDto hotViewBoards = boardQueryService.getMemberBoards(loginUserId, author.getId(), PageRequest.of(0, 10));
+		//when
+		boardQueryService.getMemberBoards(loginUser.getId(), author.getId(), pageRequest);
 
 		//then
-		then(boardRepository).should().getMemberBoards(author.getId(), PageRequest.of(0, 10));
-		assertThat(hotViewBoards.getResult().size()).isEqualTo(2);
-		assertThat(hotViewBoards.getResult().get(0).getCategories()).isEqualTo(List.of(Species.CAT, Species.DOG));
+		then(boardRepository).should().getMemberBoards(author.getId(), pageRequest);
+		then(boardMapper).should().toBoardsResponseDto(anyList(), eq(boards.size()));
 	}
 
+	@DisplayName("스크랩한 게시물을 매핑해서 컨트롤러에게 전달한다.")
 	@Test
-	@DisplayName("사용자의 ID 기준 반응, 북마크 정보를 포함한 스크랩한 게시물들을 가져온다.")
 	void getScraps() {
-		// given
-		Long loginUserId = 1L;
-		Member author = mock(Member.class);
-		Board board = getMockBoardOf(author);
-		given(author.getId()).willReturn(2L);
+		Member author = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		Member loginUser = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		List<Board> boards = List.of(
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID),
+				TestBoard.builder()
+						.member(author)
+						.build().asMockEntity(IGNORE_ID));
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		given(boardRepository.getScrapBoards(loginUser.getId(), pageRequest)).willReturn(boards);
 
-		given(boardRepository.getScrapBoards(loginUserId, PageRequest.of(0, 10)))
-				.willReturn(List.of(board, board));
-
-		// when
-		BoardsPaginationDto hotViewBoards = boardQueryService.getScraps(loginUserId, PageRequest.of(0, 10));
+		//when
+		boardQueryService.getScraps(loginUser.getId(), pageRequest);
 
 		//then
-		then(boardRepository).should().getScrapBoards(loginUserId, PageRequest.of(0, 10));
-		assertThat(hotViewBoards.getResult().size()).isEqualTo(2);
-		assertThat(hotViewBoards.getResult().get(0).getCategories()).isEqualTo(List.of(Species.CAT, Species.DOG));
+		then(boardRepository).should().getScrapBoards(loginUser.getId(), pageRequest);
+		then(boardMapper).should().toBoardsResponseDto(anyList(), eq(boards.size()));
 	}
 
+	@DisplayName("팔로잉한 멤버의 게시물을 매핑해서 컨트롤러에게 전달한다.")
 	@Test
-	@DisplayName("사용자의 ID 기준 반응, 북마크 정보를 포함한 팔로잉 중인 멤버들의 게시글들을 가져온다.")
 	void getFollowingsBoards() {
-		// given
-		Long loginUserId = 1L;
-		Member author = mock(Member.class);
-		Board board = getMockBoardOf(author);
-		given(author.getId()).willReturn(2L);
+		Member follwing = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		Member loginUser = TestMember.builder().build().asMockEntity(IGNORE_ID);
+		List<Board> boards = List.of(
+				TestBoard.builder()
+						.member(follwing)
+						.build().asMockEntity(IGNORE_ID),
+				TestBoard.builder()
+						.member(follwing)
+						.build().asMockEntity(IGNORE_ID));
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		given(boardRepository.getFollowingsBoards(loginUser.getId(), pageRequest)).willReturn(boards);
 
-		given(boardRepository.getFollowingsBoards(loginUserId, PageRequest.of(0, 10)))
-				.willReturn(List.of(board, board));
-
-		// when
-		BoardsPaginationDto hotViewBoards = boardQueryService.getFollowingsBoards(loginUserId, PageRequest.of(0, 10));
+		//when
+		boardQueryService.getFollowingsBoards(loginUser.getId(), pageRequest);
 
 		//then
-		then(boardRepository).should().getFollowingsBoards(loginUserId, PageRequest.of(0, 10));
-		assertThat(hotViewBoards.getResult().size()).isEqualTo(2);
-		assertThat(hotViewBoards.getResult().get(0).getCategories()).isEqualTo(List.of(Species.CAT, Species.DOG));
+		then(boardRepository).should().getFollowingsBoards(loginUser.getId(), pageRequest);
+		then(boardMapper).should().toBoardsResponseDto(anyList(), eq(boards.size()));
 	}
+
 }

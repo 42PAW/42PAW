7872d4ceea5764069c4eb0705a2d5efeefbe1871
hyungseok <eric.nohhyungseok@gmail.com>
2023-08-26 00:19:09 +0900

    [FE] FEAT: 비로그인 상태 언어 설정 및 동물 카테고리 업데이트를 위한 localStorage 추가

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { isRightSectionOpenedState } from "@/recoil/atom";
 import LeftMenuSection from "@/components/LeftMenuSection/LeftMenuSection";
@@ -12,22 +12,22 @@ import { getCookie } from "@/api/cookie/cookies";
 import { axiosMyInfo } from "@/api/axios/axios.custom";
 import { userInfoState } from "@/recoil/atom";
 import { UserInfoDTO } from "@/types/dto/member.dto";
-import { languageState } from "@/recoil/atom";
 import useTranslator from "@/hooks/useTranslator";
+import { Language } from "@/types/enum/language.enum";
 
 const Layout = () => {
   const location = useLocation();
   const token = getCookie("access_token");
+  const localLanguage = localStorage.getItem("language");
   const [userInfo, setUserInfo] = useRecoilState<UserInfoDTO | null>(
     userInfoState
   );
-  const setLanugage = useSetRecoilState(languageState);
   const [isRightSectionOpened] = useRecoilState<boolean>(
     isRightSectionOpenedState
   );
   const { translator } = useTranslator();
 
-  /**메인 화면일 때만 게시글 정렬 버튼 보여주기*/
+  //메인 화면일 때만 게시글 정렬 버튼 보여주기
   const isMainPage: boolean = location.pathname === "/";
   const isSignUpPage: boolean = location.pathname === "/sign-up";
   const isProfilePage: boolean =
@@ -44,9 +44,10 @@ const Layout = () => {
   };
 
   useEffect(() => {
-    if (token && !userInfo) {
-      getMyInfo();
-    }
+    //로그인 성공 시 유저 정보 받아오기
+    if (token && !userInfo) getMyInfo();
+    //비로그인 상태에서 로컬 스토리지 키값을 통해 언어 설정
+    if (!token && localLanguage) translator(localLanguage as Language);
   }, []);
 
   return isSignUpPage ? (

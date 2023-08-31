0cf3da1195189ef5f5bfd6bff706675e2c7742ee
Ssuamje <side2850@naver.com>
2023-08-31 19:06:06 +0900

    [BE] FIX: 켜져있는 spring boot를 종료하고 새 jar를 실행하도록 deploy.sh 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,2 +1,7 @@
-cd /home/ec2-user/deploy
-echo "hello world!" > deploy_success.txt
+#!/bin/bash
+USER_HOME=/home/ec2-user
+PROJECT_NAME=pet
+SPRING_BOOT_PROCESS_ID= $(lsof | grep '.jar' | awk '{print $2}' | head -n 1)
+
+kill $SPRING_BOOT_PROCESS_ID
+java -jar -Dspring.profiles.active=prod ./deploy/pet-*.jar &

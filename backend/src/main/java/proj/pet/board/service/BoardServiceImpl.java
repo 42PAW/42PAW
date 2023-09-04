a7bc4de09ac489fcc5e92357308f708d196a4174
jiwon <jwpark0119@gmail.com>
2023-09-04 11:27:24 +0900

    [BE] FIX: 게시글 가져오기 중 block과 category 필터링 해서 조회하도록 수정

--- backend/src/main/java/proj/pet/board/service/BoardServiceImpl.java
+++ backend/src/main/java/proj/pet/board/service/BoardServiceImpl.java
@@ -1,30 +1,36 @@
 package proj.pet.board.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import proj.pet.board.domain.*;
-import proj.pet.board.repository.BoardCategoryFilterRepository;
-import proj.pet.board.repository.BoardMediaRepository;
-import proj.pet.board.repository.BoardRepository;
-import proj.pet.category.domain.AnimalCategory;
-import proj.pet.category.domain.BoardCategoryFilter;
-import proj.pet.category.domain.Species;
-import proj.pet.category.repository.AnimalCategoryRepository;
-import proj.pet.comment.repository.CommentRepository;
-import proj.pet.exception.ExceptionStatus;
-import proj.pet.exception.ServiceException;
-import proj.pet.member.domain.Member;
-import proj.pet.member.repository.MemberRepository;
+import static proj.pet.exception.ExceptionStatus.ALREADY_DELETED_BOARD;
+import static proj.pet.exception.ExceptionStatus.NOT_FOUND_BOARD;
+import static proj.pet.exception.ExceptionStatus.NOT_FOUND_MEMBER;
+import static proj.pet.exception.ExceptionStatus.UNAUTHENTICATED;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
-import static proj.pet.exception.ExceptionStatus.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import proj.pet.board.domain.Board;
+import proj.pet.board.domain.BoardMedia;
+import proj.pet.board.domain.BoardMediaManager;
+import proj.pet.board.domain.MediaType;
+import proj.pet.board.domain.VisibleScope;
+import proj.pet.board.repository.BoardMediaRepository;
+import proj.pet.board.repository.BoardRepository;
+import proj.pet.category.domain.AnimalCategory;
+import proj.pet.category.domain.BoardCategoryFilter;
+import proj.pet.category.domain.Species;
+import proj.pet.category.repository.AnimalCategoryRepository;
+import proj.pet.category.repository.BoardCategoryFilterRepository;
+import proj.pet.comment.repository.CommentRepository;
+import proj.pet.exception.ExceptionStatus;
+import proj.pet.exception.ServiceException;
+import proj.pet.member.domain.Member;
+import proj.pet.member.repository.MemberRepository;
 
 /**
  * Board의 CUD 비즈니스 로직을 담당하는 서비스 구현체
@@ -64,8 +70,10 @@ public class BoardServiceImpl implements BoardService {
 		Board board = boardRepository.save(
 				Board.of(member, VisibleScope.PUBLIC, content, now));
 
-		List<AnimalCategory> animalCategories = animalCategoryRepository.findBySpeciesIn(speciesList);
-		List<BoardCategoryFilter> categoryFilters = convertToBoardCategoryFilters(animalCategories, board);
+		List<AnimalCategory> animalCategories = animalCategoryRepository.findBySpeciesIn(
+				speciesList);
+		List<BoardCategoryFilter> categoryFilters = convertToBoardCategoryFilters(animalCategories,
+				board);
 		categoryFilters = boardCategoryFilterRepository.saveAll(categoryFilters);
 		board.addCategoryFilters(categoryFilters);
 
@@ -76,7 +84,8 @@ public class BoardServiceImpl implements BoardService {
 		return boardRepository.save(board);
 	}
 
-	private List<BoardCategoryFilter> convertToBoardCategoryFilters(List<AnimalCategory> animalCategories, Board board) {
+	private List<BoardCategoryFilter> convertToBoardCategoryFilters(
+			List<AnimalCategory> animalCategories, Board board) {
 		return animalCategories.stream()
 				.map(category -> BoardCategoryFilter.of(board, category))
 				.toList();
@@ -86,7 +95,8 @@ public class BoardServiceImpl implements BoardService {
 		AtomicInteger index = new AtomicInteger(0);
 		List<BoardMedia> mediaList = mediaDataList.stream()
 				.map(data -> {
-					String mediaUrl = boardMediaManager.uploadMedia(data, UUID.randomUUID().toString());
+					String mediaUrl = boardMediaManager.uploadMedia(data,
+							UUID.randomUUID().toString());
 					return BoardMedia.of(board, mediaUrl, index.getAndIncrement(),
 							MediaType.from(data));
 				}).collect(Collectors.toList());

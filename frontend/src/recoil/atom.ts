f7feb1ab98ed5616ce287bf3ff852e809ad6f0f0
hyungseok <eric.nohhyungseok@gmail.com>
2023-09-08 08:40:19 +0900

    Revert "Revert "Revert "[FE] FEAT: logoClickObserver 추가 -> 로고 클릭시 위로 스크롤"""
    This reverts commit 7fed214f2c3d6b8b884a111c65bbd42582af274d.

--- frontend/src/recoil/atom.ts
+++ frontend/src/recoil/atom.ts
@@ -139,8 +139,3 @@ export const boardsLengthState = atom<number>({
   key: "boardsLength",
   default: 10000,
 });
-
-export const logoClickObserverState = atom<number>({
-  key: "logoClickObserver",
-  default: 0,
-});

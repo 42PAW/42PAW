27414a9c4d4df65b29cf664cadcdb08d9d278348
Ssuamje <side2850@naver.com>
2023-11-06 15:35:50 +0900

    FIX: 수정버전을 기존의 버전으로 이름 변경 및 기존 API 삭제, 테스트 수정

--- backend/src/main/java/proj/pet/board/dto/BoardCreateRequestDto.java
+++ backend/src/main/java/proj/pet/board/dto/BoardCreateRequestDto.java
@@ -3,20 +3,18 @@ package proj.pet.board.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.NoArgsConstructor;
 import proj.pet.category.domain.Species;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
-@ToString
-public class BoardCreateRequestDto {
-
+public class BoardCreateRequestDto2 {
 	@NotNull
-	private final List<MultipartFile> mediaDataList;
+	private List<String> mediaUrlList;
 	@NotNull
-	private final List<Species> categoryList;
-	private final String content;
+	private List<Species> categoryList;
+	private String content;
 }
